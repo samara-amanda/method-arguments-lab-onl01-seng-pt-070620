9ec29af69f8476956e739d7891a4e0610f38ad7c
Samara Grant <samara.amanda01@gmail.com>
2020-07-07 00:27:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,4 @@
 def introduction(name, language)
   name = "Dan"
-  language = "Ember.js"
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name}."
 end
